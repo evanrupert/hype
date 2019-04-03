1c7dedad51cd7371866f7f1b95810847786a2341
Evan Rupert <rupertevanr@gmail.com>
2019-04-02 21:43:16 -0400

    Added todos

--- src/main/kotlin/com/hype/Application.kt
+++ src/main/kotlin/com/hype/Application.kt
@@ -6,6 +6,10 @@ import org.springframework.boot.runApplication
 @SpringBootApplication
 class HypeApplication
 
+// TODO: Connect to test database when running tests
+// TODO: Finish CRUD for users/items/transactions
+// TODO: Implement flyway in order to handle database migrations
+
 fun main(args: Array<String>) {
     HypeDatabase.connect
     runApplication<HypeApplication>(*args)
