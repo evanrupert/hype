45acaa303cedee654eb7d5d3cbade81838838d5b
Evan Rupert <rupertevanr@gmail.com>
2019-04-02 20:08:29 -0400

    Created default gradle task and configured RequestMapping

--- src/main/kotlin/com/hype/greeting/GreetingController.kt
+++ src/main/kotlin/com/hype/greeting/GreetingController.kt
@@ -1,9 +1,11 @@
 package com.hype.greeting
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@RestController("/greeting")
+@RestController
+@RequestMapping("greeting")
 class GreetingController {
     @GetMapping
     fun greet(): String = "Hello, World!"
